695fd399a02932149ffe2535871f0fb84d1a18fe
Abhishek Kumar Goswami <37690947+abhi1615@users.noreply.github.com>
2019-03-07 19:30:53 +0530

    Delete problem8.py

--- Hunter/Set-01/problem8.py
+++
@@ -1,7 +0,0 @@
-n = int(input())
-list = list(map(int,input().split()))
-for i in range(n):
-    for j in range(i+1,n):
-        if (list[i]+list[j]) in list:
-            print(list[i], list[j], list[i]+list[j])
-
